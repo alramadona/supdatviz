f8d1445626d92442114de6f07f58e59883aff6a5
Aditya Ramadona <alramadona@ugm.ac.id>
2025-02-15 17:56:04 +0700

    Update s3.R
    prepare for MPAD 2025 bos.!

--- s3.R
+++ s3.R
@@ -1,101 +1,154 @@
-library(rtweet)
-library(ggplot2)
 
+# load dataset ------------------------------------------------------------
+
+## sumber: https://doi.org/10.1371/journal.pone.0152688
+datTotal <- read.csv("dat/dengue.csv", na.string="#N/A")
+
+names(datTotal)
+str(datTotal)
+
+# library
 library(dplyr)
-library(stringi)
-library(wordcloud)
-
-auth_setup_default()
-
-?search_tweets
-tw_fkkmk <- search_tweets("kopi", n = 10000,
-                          retryonratelimit = T,
-                          include_rts = FALSE)
-
-saveRDS(tw_fkkmk, "dat/tw_fkkmk.rds")
-
-ts_plot(tw_fkkmk) +
-  theme_minimal() +
-  theme(plot.title = element_text(face = "bold")) +
-  labs(
-    x = NULL, y = NULL,
-    title = "Frequency of 'teh' Twitter statuses from past 9 days",
-    subtitle = "Twitter status (tweet) counts aggregated using three-hour intervals",
-    caption = "Source: Data collected from Twitter's REST API via rtweet"
-  )
+library(zoo)
+library(ggplot2)
+library(ggpubr)
 
 
-# analisis teks -----------------------------------------------------------
+# membuat dataset baru ----------------------------------------------------
 
-comments_text <- as.character(tw_fkkmk$full_text)
-comments_text = tibble(text = Reduce(c, comments_text)) %>%
-  mutate(text = stri_trans_general(tolower(text), "Latin-ASCII"))
+dat <- select(datTotal, year,month,
+              denguel0,templ0,rainl0,
+              denguel1,templ1,rainl1,
+              denguel2,templ2,rainl2,
+              denguel3,templ3,rainl3)
 
-remove = c("ada","adanya","adalah","adapun","agak","agaknya","agar",
-           "akan","akankah","akhirnya","aku","akulah","amat","amatlah",
-           "anda","andalah","antar","diantaranya","antara","antaranya",
-           "diantara","apa","apaan","mengapa","apabila","apakah",
-           "apalagi","apatah","atau","ataukah","ataupun","bagai",
-           "bagaikan","sebagai","sebagainya","bagaimana","bagaimanapun",
-           "sebagaimana","bagaimanakah","bagi","bahkan","bahwa",
-           "bahwasanya","sebaliknya","banyak","sebanyak","beberapa",
-           "seberapa","begini","beginian","beginikah","beginilah",
-           "sebegini","begitu","begitukah","begitulah","begitupun",
-           "sebegitu","belum","belumlah","sebelum","sebelumnya",
-           "sebenarnya","berapa","berapakah","berapalah","berapapun",
-           "betulkah","sebetulnya","biasa","biasanya","bila","bilakah",
-           "bisa","bisakah","sebisanya","boleh","bolehkah","bolehlah",
-           "buat","bukan","bukankah","bukanlah","bukannya","cuma",
-           "percuma","dahulu","dalam","dan","dapat","dari","daripada",
-           "dekat","demi","demikian","demikianlah","sedemikian","dengan",
-           "depan","di","dia","dialah","dini","diri","dirinya","terdiri",
-           "dong","dulu","enggak","enggaknya","entah","entahlah",
-           "terhadap","terhadapnya","hal","hampir","hanya","hanyalah",
-           "harus","haruslah","harusnya","seharusnya","hendak",
-           "hendaklah","hendaknya","hingga","sehingga","ia","ialah",
-           "ibarat","ingin","inginkah","inginkan","ini","inikah",
-           "inilah","itu","itukah","itulah","jangan","jangankan",
-           "janganlah","jika","jikalau","juga","justru","kala","kalau",
-           "kalaulah","kalaupun","kalian","kami","kamilah","kamu",
-           "kamulah","kan","kapan","kapankah","kapanpun","dikarenakan",
-           "karena","karenanya","ke","kecil","kemudian","kenapa",
-           "kepada","kepadanya","ketika","seketika","khususnya","kini",
-           "kinilah","kiranya","sekiranya","kita","kitalah","kok","lagi",
-           "lagian","selagi","lah","lain","lainnya","melainkan","selaku",
-           "lalu","melalui","terlalu","lama","lamanya","selama","selama",
-           "selamanya","lebih","terlebih","bermacam","macam","semacam",
-           "maka","makanya","makin","malah","malahan","mampu","mampukah",
-           "mana","manakala","manalagi","masih","masihkah","semasih",
-           "masing","mau","maupun","semaunya","memang","mereka",
-           "merekalah","meski","meskipun","semula","mungkin","mungkinkah",
-           "nah","namun","nanti","nantinya","nyaris","oleh","olehnya",
-           "seorang","seseorang","pada","padanya","padahal","paling",
-           "sepanjang","pantas","sepantasnya","sepantasnyalah","para",
-           "pasti","pastilah","per","pernah","pula","pun","merupakan",
-           "rupanya","serupa","saat","saatnya","sesaat","saja","sajalah",
-           "saling","bersama","sama","sesama","sambil","sampai","sana",
-           "sangat","sangatlah","saya","sayalah","se","sebab","sebabnya",
-           "sebuah","tersebut","tersebutlah","sedang","sedangkan",
-           "sedikit","sedikitnya","segala","segalanya","segera",
-           "sesegera","sejak","sejenak","sekali","sekalian","sekalipun",
-           "sesekali","sekaligus","sekarang","sekarang","sekitar",
-           "sekitarnya","sela","selain","selalu","seluruh","seluruhnya",
-           "semakin","sementara","sempat","semua","semuanya","sendiri",
-           "sendirinya","seolah","seperti","sepertinya","sering",
-           "seringnya","serta","siapa","siapakah","siapapun","disini",
-           "disinilah","sini","sinilah","sesuatu","sesuatunya","suatu",
-           "sesudah","sesudahnya","sudah","sudahkah","sudahlah","supaya",
-           "tadi","tadinya","tak","tanpa","setelah","telah","tentang",
-           "tentu","tentulah","tentunya","tertentu","seterusnya","tapi",
-           "tetapi","setiap","tiap","setidaknya","tidak","tidakkah",
-           "tidaklah","toh","waduh","wah","wahai","sewaktu","walau",
-           "walaupun","wong","yaitu","yakni","yang")
-words = tibble(word = Reduce(c, stri_extract_all_words(comments_text$text))) %>%
-  group_by(word) %>% count() %>% arrange(desc(n)) %>% filter(nchar(word) >= 3) %>%
-  filter(n > 2 & word %in% remove == FALSE) 
 
-words <- words[c(1:500),]
+# membuat variabel baru ---------------------------------------------------
 
-set.seed(3)
-wordcloud(words$word, words$n, random.order = FALSE, random.color = TRUE,
-          rot.per = 0.3, colors = 1:nrow(words))
+dat$term <- rep(rep(c(1,2), each=6),13)
+dat$YM <- paste(dat$month, dat$year, sep="-")
+dat$YM <- as.yearmon(dat$YM, format="%m-%Y")
+dat$date <- as.Date(dat$YM)
+dat$MoY <- as.factor(dat$month)
+
+
+# rename variables --------------------------------------------------------
+
+names(dat)
+names(dat) <- c("year", "month", 
+                "cases", "temp", "rain",
+                "cases_l1", "temp_l1", "rain_l1",
+                "cases_l2", "temp_l2", "rain_l2",
+                "cases_l3", "temp_l3", "rain_l3",
+                "term", "YM", "date", "MoY")
+str(dat)
+dat$n <- c(1:nrow(dat))
+
+# visualisasi data --------------------------------------------------------
+
+q1 <- ggplot(dat, aes(x = MoY, y = cases)) +
+  geom_boxplot() + theme_bw()
+q1
+
+q2 <- ggplot(dat, aes(x = MoY, y = temp)) +
+  geom_boxplot() + theme_bw()
+q2
+
+q3 <- ggplot(dat, aes(x = MoY, y = rain)) +
+  geom_boxplot() + theme_bw()
+q3
+
+ggarrange(q1, q2, q3,
+          labels = c("A", "B", "C"),
+          ncol = 1, nrow = 3)
+
+
+# dataset [train & test] --------------------------------------------------
+
+train <- subset(dat, YM<"Jan 2011")
+test <- subset(dat, YM>="Jan 2011")
+
+
+# modeling - linear regression --------------------------------------------
+
+model.temp <- glm(cases~temp, data=train, family="gaussian", na.action=na.exclude)
+summary(model.temp)
+
+##
+
+model.rain <- glm(cases~rain, data=train, family="gaussian", na.action=na.exclude)
+summary(model.rain)
+
+### calculate McFadden's R-squared for model
+with(summary(model.rain), 1 - deviance/null.deviance)
+
+##
+
+model.rain_l1 <- glm(cases~rain_l1, data=train, family="gaussian", na.action=na.exclude)
+summary(model.rain_l1)
+
+### calculate McFadden's R-squared for model
+with(summary(model.rain_l1), 1 - deviance/null.deviance)
+
+##
+
+model.rain_l2 <- glm(cases~rain_l2, data=train, family="gaussian", na.action=na.exclude)
+summary(model.rain_l2)
+
+### calculate McFadden's R-squared for model
+with(summary(model.rain_l2), 1 - deviance/null.deviance)
+
+##
+
+model.rain_l3 <- glm(cases~rain_l3, data=train, family="gaussian", na.action=na.exclude)
+summary(model.rain_l3)
+
+### calculate McFadden's R-squared for model
+with(summary(model.rain_l3), 1 - deviance/null.deviance)
+
+### visualize
+par(mfrow=c(2,2))
+
+# model 1
+plot(train$n, train$cases, type="l", ylab="Number Cases", axes=F, xlab="Year")
+points(predict(model.rain, type="response"),type="l", col="red")
+axis(1, at=c(6,18,30,42,54,66,78,90,102,114),labels=c(2001:2010))
+axis(2, at=c(0,50,100,150,200,250))
+title(main="Model A")
+
+# model 2
+plot(train$n, train$cases, type="l", ylab="Number Cases", axes=F, xlab="Year")
+points(predict(model.rain_l1, type="response"),type="l", col="red")
+axis(1, at=c(6,18,30,42,54,66,78,90,102,114),labels=c(2001:2010))
+axis(2, at=c(0,50,100,150,200,250))
+title(main="Model B")
+
+# model 3
+plot(train$n, train$cases, type="l", ylab="Number Cases", axes=F, xlab="Year")
+points(predict(model.rain_l2, type="response"),type="l", col="red")
+axis(1, at=c(6,18,30,42,54,66,78,90,102,114),labels=c(2001:2010))
+axis(2, at=c(0,50,100,150,200,250))
+title(main="Model C")
+
+# model 4
+plot(train$n, train$cases, type="l", ylab="Number Cases", axes=F, xlab="Year")
+points(predict(model.rain_l3, type="response"),type="l", col="red")
+axis(1, at=c(6,18,30,42,54,66,78,90,102,114),labels=c(2001:2010))
+axis(2, at=c(0,50,100,150,200,250))
+title(main="Model D")
+
+
+# modeling - linear regression (validation) -------------------------------
+
+dat$predict <- predict(model.rain_l2, type="response", newdata=dat)
+
+train <- subset(dat, YM<"Jan 2011")
+test <- subset(dat, YM>="Jan 2011")
+
+par(mfrow=c(1,1))
+plot(dat$n, dat$cases, type="l", ylab="Number Cases", axes=F, xlab="Year")
+points(train$n, train$predict, type="l", col="red")
+points(test$n, test$predict, type="l", col="blue")
+axis(1, at=c(6,18,30,42,54,66,78,90,102,114,126,138,150),labels=c(2001:2013))
+axis(2, at=c(0,50,100,150,200,250))
+# title(main="")
